4e665d7961c921c21d2fd56830e18689bc1e1301
Samuele S <samisama2002@gmail.com>
2018-08-17 18:16:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function happyHolidays() {
 function happyHolidaysTo(nome) {
   return `Happy holidays, ${name}!`;
 }
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}`;
+}
